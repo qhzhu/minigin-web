4c3f5cd81b89668af8731f60667921d0cdc3b89b
qzhu <qzhu@qzhus-MacBook-Pro.local>
2023-08-15 23:20:24 +0900

    Add HTML and JSON handling method

--- minigin/context.go
+++ minigin/context.go
@@ -1,9 +1,12 @@
 package minigin
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type H map[string]interface{}
 
 type Context struct {
 	// original
@@ -34,8 +37,36 @@ func (c *Context) SetHeader(key string, value string) {
 	c.Writer.Header().Set(key, value)
 }
 
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
 func (c *Context) String(code int, format string, values ...interface{}) {
 	c.Status(code)
 	c.SetHeader("Content-Type", "text/plain")
 	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
 }
